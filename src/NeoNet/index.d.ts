c9136ecc762378a862dbfae53034ab74fd6be7aa
NeonD00m <neonfamxxi@gmail.com>
2022-11-27 12:52:53 -0800

    ts update for v1.3.0

--- src/NeoNet/index.d.ts
+++ src/NeoNet/index.d.ts
@@ -24,7 +24,7 @@ declare namespace NeoNet {
     export type Fire = (name: string, ...args: any) => undefined
     export type FireFor = (name: string, player: Player, ...args: any) => undefined
     export type FireExcept = (name: string, player: Player, ...args: any) => undefined
-    export type FireList = (name: string, players: {Player}, ...args: any) => undefined
+    export type FireList = (name: string, players: {}, ...args: any) => undefined
 
     export type ConnectOnce = (name: string, handler: (...args: any) => undefined, middleware: RemoteServerMiddleware | RemoteClientMiddleware) => undefined
     export type ConnectUntil = (name: string, handler: (...args: any) => undefined, middleware: RemoteServerMiddleware | RemoteClientMiddleware) => undefined
@@ -34,12 +34,20 @@ declare namespace NeoNet {
     export type Connect = (name: string, handler: (...args: any) => undefined, middleware: RemoteServerMiddleware | RemoteClientMiddleware) => RBXScriptConnection
     export type Handle = (name: string, handler: (player: Player, ...args: any) => LuaTuple<[...args: any]>) => undefined
     export type SetValue = (name: string, value: any) => undefined
+    export type SetTop = (name: string, value: any) => undefined
+    export type SetFor = (name: string, player: Player, value: any) => undefined
+    export type SetList = (name: string, players: {}, value: any) => undefined
+    export type ClearValues = (name: string) => undefined
+    export type ClearValueFor = (name: string, player: Player) => undefined
+    export type ClearValuesList = (name: string, players: {}) => undefined
+    export type GetFor = (name: string, player: Player) => any
     export type Clean = (undefined)
 
     // client-only
     //skip over connect on client :sob:
     export type Observe = (name: string, handler: (value: any) => undefined, middleware: RemoteClientMiddleware) => RBXScriptConnection
     export type Invoke = (name: string, ...args: any) => LuaTuple<[...args: any]>
+    export type IsValueReady = (name: string) => boolean
 }
 
 export = NeoNet
